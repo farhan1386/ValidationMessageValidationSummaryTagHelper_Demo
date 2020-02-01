8fe4adfb3c199312186fd35fa8bad8fbbda505c8
Farhan Ahmed <farhan1086@gmail.com>
2020-02-01 13:59:06 +0530

    initial commit

---
+++ ValidationMessageValidationSummaryTagHelper_Demo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValidationMessageValidationSummaryTagHelper_Demo.Models;
+
+namespace ValidationMessageValidationSummaryTagHelper_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+    }
+}
